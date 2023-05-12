8ff98a560b7847d43379c22f3d6af72e5e0947e2
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-05-11 20:22:07 -0500

    Terminado

--- anime/anime/settings.py
+++ anime/anime/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-vm=i$pdstyo_nm=(q(6oc0osq&4!23t*r#1a$)uzj)$k5y9^gs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['anime-galaxy.onrender.com']
 
 # Application definition
 
